6ac84d6d9fbe379bbfec9d0234a82b72f7b83528
Devon Lumsden <devonlumsden@mail.com>
2019-06-06 20:45:12 +1200

    get api working

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
 
-const App = () => {
-  return (
-    <h1>React development has begun!</h1>
-  )
+import { callCountryByName } from '../api'
+
+class App extends React.Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      countries: ''
+    }
+  }
+  componentDidMount() {
+    callCountryByName('New Zealand')
+  }
+
+  render(){
+    return (
+      <h1>React development has begun!</h1>
+    )
+  }
 }
 
 export default App
